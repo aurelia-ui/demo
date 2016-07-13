fb2c6387a9e77e74bba007763b64cdd95c226059
Pavel <Pavel.Shabalin@badirah.ae>
2016-07-12 23:37:24 -0700

    added welcome page

--- src/welcome.js
+++ src/welcome.js
@@ -1,5 +1,10 @@
 export class Welcome {
 
-
+  
+  activate(){
+    SystemJS.import("dropdown/dropdown.html" + "!text").then(html => {
+      this.htmlCode = Prism.highlight(html, Prism.languages.html);
+    })
+  }  
 }
 
